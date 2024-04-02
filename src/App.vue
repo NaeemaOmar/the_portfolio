feb0e8f0f9b4f805db6eb35d54ac8bc49d2923e4
Naeema <naeemaomar19@gmail.com>
2024-04-02 16:34:54 +0200

    navbar+footer styling + beginnings of contactPg

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,90 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  </nav> -->
+    <NavBar />
+      <div class="mainCreamBg mx-auto topMargin">
+        <router-view />
+        <Footer />
+      </div>
+    <!-- BELOW = ALT STYLING. NOTE: mainCreamBg below has a width of 95% -->
+    <!-- <div class="darkGreenBg-Gradient">
+      <div class="mainCreamBg mx-auto topMargin">
+        <router-view />
+      </div>
+      <Footer />
+
+    </div> -->
 </template>
 
+<script>
+import NavBar from "@/components/NavBar.vue";
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    NavBar,
+    Footer
+  },
+};
+</script>
+
 <style>
+/* GENERAL STYLING */
+:root {
+  --mainDarkGreen: #051808;
+  --mainCream: #ffe49f;
+  --white: #ffffff;
+  --black: #000000;
+}
+
+.topMargin{
+  margin-top: 92px;
+}
+
+.testBorder{
+  border: dotted;
+  border-color: aqua;
+}
+
+.testBg{
+  background-color: aqua;
+}
+
+.testColor{
+  color: aqua;
+}
+
+.darkGreenBg{
+  background-color: var(--mainDarkGreen);
+}
+
+.mainCreamBg{
+  background-color: var(--mainCream);
+  /* width: 90%; */
+}
+
+.darkGreenBg-Gradient{
+  background-image: radial-gradient(var(--mainCream), var(--mainDarkGreen));
+}
+
+.mainCreamBorder{
+  border: solid;
+  border-color: var(--mainCream);
+  border-radius: 5px ;
+}
+
+.creamTxt{
+  color: var(--mainCream);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: var(--mainDarkGreen);
 }
 
 nav {
@@ -21,10 +93,10 @@ nav {
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: var(--black);
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: var(--mainCream);
 }
 </style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+    <h1>This is the contact pg</h1>
+
+    <div class="greenDiv">
+        <h3>Get in touch</h3> 
+        <div class="row">
+            <div class="col-3">
+                <p class="mx-auto">icon</p>
+            </div>
+            <div class="col-9">
+                <p>Bellville, Cape Town, 7530</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <p class="mx-auto">icon</p>
+            </div>
+            <div class="col-9">
+                <p>naeemaomar19@gmail.com</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <p class="mx-auto">icon</p>
+            </div>
+            <div class="col-9">
+                <p>071 000 000</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                gitHub+linkedIn icons
+            </div>
+        </div>
+    </div>
+    <div class="theForm">
+        <p>Name</p>
+    </div>
+</div>
+</template>
+
+<style>
+
+.greenDiv{
+    background-color: var(--mainDarkGreen);
+    color: var(--mainCream);
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px 10px var(--mainDarkGreen);
+    margin-block: 50px;
+    margin-inline-start: 20px;
+    width: 35%; /*ALTERNATIVELY: use row w/ col-4 vs col-8. col-4 will have a green div w/ a margin 20px allround*/ 
+    /* UPDATE: defs need to use row otherwise i'll need to use grids to get the stuff in 1 line */
+}
+
+</style>
